277733bcff2805dfb92cd12e4511f81a3a32248c
Pikselle <Maureen.metge@gmail.com>
2021-01-19 19:37:56 +0100

    Navbar
    Début boutons barre de nav

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,1 +1,18 @@
 import './body.html';
+import './presentation_projet.js';
+
+
+Session.set('templateAAfficher', '');
+
+
+Template.body.helpers({
+    currentTrucaffiche: function(truc){
+      return Session.get('templateAAfficher');
+    },
+});
+
+Template.body.events({
+    'click .presentation'(event){
+     Session.set('templateAAfficher', 't_presentation');
+    }
+});
---
+++ imports/ui/presentation_projet.js
@@ -0,0 +1,1 @@
+import './presentation_projet.html';
