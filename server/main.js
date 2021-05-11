da1bb3a0f0c5248d3f5bb85eb41fcf63bc181c11
JL-lamo <59889607+JL-lamo@users.noreply.github.com>
2021-05-11 20:15:48 +0200

    zdsf

--- server/main.js
+++ server/main.js
@@ -2,6 +2,21 @@
 import  '../imports/api/annonce.js';
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
   // code to run on server at startup
+
+import { Accounts } from 'meteor/accounts-base';
+
+
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
 });
