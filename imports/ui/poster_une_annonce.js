f45c026a1c9e308e32d20f56e3418adfc4f8d1f8
maximeduret <max.duret75@gmail.com>
2021-06-01 19:34:21 +0200

    post annonce

--- imports/ui/poster_une_annonce.js
+++ imports/ui/poster_une_annonce.js
@@ -11,9 +11,10 @@ Template.t_post_annonce.events({
     const categorie_de_l_annonce = target.categorie;
     const qualifications_requises = target.qualifications;
     const decription_de_l_annonce = target.description;
-
+    const utilisateur  = "michel";
+    const date_post_annonce ="18 juin";
 // Appel à une méthode qu'il va falloir créer : annonces.ajouter !
-    Meteor.call('t_post_annonce', nom_de_annonce, categorie_de_l_annonce, qualifications_requises, description_de_l_annonce);
+    Meteor.call('annonces.ajouter', nom_de_annonce, categorie_de_l_annonce, qualifications_requises, description_de_l_annonce, utilisateur, date_post_annonce);
 
   }
 
